612d407f0c0d38a7c3c22c805ae6c6acf68bf320
mcrajip <muchamadrajif@gmail.com>
2022-11-26 16:28:35 +0700

    add doc

--- main.js
+++ main.js
@@ -10,7 +10,29 @@ function toggleDetail(e) {
     
     const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
+    const detail2 = $(item).children(".about-exp-item-detail-li")
     $(detail).slideToggle()
+    $(detail2).slideToggle()
     
 }
 
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if(!$(email).val()) {
+        alert("email is required")
+    } else if (!$(subject).val()) {
+        alert("Subject is required")
+    } else if (!$(message).val()) {
+        alert("Message is Required")
+    } else {
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+        alert("Form Submitted")
+    }
+
+}
